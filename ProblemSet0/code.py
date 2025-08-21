ec24740d8014cd04fe5f2d0033d06e6fbbe96194
Ganiu <omowalefst@gmail.com>
2025-08-21 17:14:36 +0300

    new repo

---
+++ ProblemSet0/code.py
@@ -0,0 +1,15 @@
+### Task 1.
+> [!NOTE]
+> In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+```python
+def main():
+    m = int(input("What's the mass of the object? "))
+    c = int(300000000)
+    E = m * square(c)
+    print(f"Energy (E) = {E} joules")
+
+def square(n):
+    return pow(n, 2)
+
+main()
+```
