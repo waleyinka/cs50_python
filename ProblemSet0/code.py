8f9c7cbf09d60c246d40a18d355b0f65e3402024
Ganiu <omowalefst@gmail.com>
2025-08-21 17:20:08 +0300

    update

--- ProblemSet0/code.py
+++ ProblemSet0/code.py
@@ -1,7 +1,7 @@
 # Task 4.
-# [EINSTEIN]
-# In a file called einstein.py, implement a program in Python that prompts the user for mass as an
-# integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+"""[EINSTEIN]
+In a file called einstein.py, implement a program in Python that prompts the user for mass as an
+integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer."""
 
 def main():
     m = int(input("What's the mass of the object? "))
