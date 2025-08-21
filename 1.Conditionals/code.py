a915fd5b83669a583b4631147d7b5ff21162d399
Ganiu Odeyinka <31272757+waleyinka@users.noreply.github.com>
2025-08-21 18:13:51 +0300

    Update code.py

--- 1.Conditionals/code.py
+++ 1.Conditionals/code.py
@@ -36,8 +36,8 @@ main()
 
 #[TASK 3 - FILE EXTENSION]
 
-#In a file called extensions.py, implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends,
-# case-insensitively, in any of these suffixes:
+#In a file called extensions.py, implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name
+#ends, case-insensitively, in any of these suffixes:
 #.gif
 #.jpg
 #.jpeg
